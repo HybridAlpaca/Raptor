091a02f468ee4120ef89c4e97b11007f222018d8
cellman1234 <sethtraman@gmail.com>
2018-06-10 15:47:18 -0500

    Minor changes

--- Engine/Source/Runtime/Graphics/Model/ModelLoader.h
+++ Engine/Source/Runtime/Graphics/Model/ModelLoader.h
@@ -1,1 +1,18 @@
 #pragma once
+
+namespace Graphics::Model
+{
+
+class Model
+{
+
+public:
+
+	Model ();
+	Model (const Model & copy) = delete;
+	~Model ();
+
+	Model & operator= (const Model & rhs) = delete;
+};
+
+}
