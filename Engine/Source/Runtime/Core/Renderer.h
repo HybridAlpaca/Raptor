fce85360d6d6d7c285add8422393b61cf3ce27f8
cellman1234 <sethtraman@gmail.com>
2018-08-08 19:45:37 -0500

    Improved documentation

--- Engine/Source/Runtime/Core/Renderer.h
+++ Engine/Source/Runtime/Core/Renderer.h
@@ -3,6 +3,19 @@
 #include "Mesh.h"
 #include "Shader.h"
 
+/**
+ *
+ * @class Renderer
+ * @headerfile Renderer.h <Core/Renderer.h>
+ * @author Seth Traman
+ * @date August 2018
+ *
+ * @brief Simple OpenGL renderer designed to render a textured mesh
+ *
+ * @deprecated Renderer can be expected to be factored out in a near-future update.  This will be accompanied by several other rendering-related changes.
+ *
+ **/
+
 class Renderer
 {
 
@@ -11,8 +24,8 @@ public:
 	Renderer ();
 	Renderer (const Renderer & copy) = delete;
 	~Renderer ();
-	
+
 	Renderer & operator= (const Renderer & copy) = delete;
-	
+
 	void Render (const Mesh & mesh, const Shader & shader) const;
 };
--- Engine/Source/Runtime/Graphics/CommandBuffer.h
+++ Engine/Source/Runtime/Graphics/CommandBuffer.h
@@ -7,10 +7,12 @@ namespace Graphics
 
 /**
  *
- * @namespace Commands
+ * @namespace Graphics::Commands
  * @headerfile CommandBuffer.h <Graphics/CommandBuffer.h>
  * @author Seth Traman
  * @date Aug 2018
+ *
+ * @brief Contains data required for DeferredCommand creation
  *
  **/
 
@@ -19,7 +21,7 @@ namespace Commands
 
 	/**
 	 *
-	 * @enum Commands::Type
+	 * @enum Type
 	 * @headerfile CommandBuffer.h <Graphics/CommandBuffer.h>
 	 * @author Seth Traman
 	 * @date Aug 2018
@@ -29,13 +31,28 @@ namespace Commands
 	 *
 	 **/
 
-	enum Type : unsigned int
+	enum class Type
 	{
-		CLEAR,
-		DRAW,
-		DRAW_INDEXED,
+		CLEAR,								///< Clears a given framebuffer
+		DRAW,									///< Draws a VAO without index buffer
+		DRAW_INDEXED,					///< Draws a VAO with index buffer
 
-		BIND_SHADER_PROGRAM
+		BIND_SHADER_PROGRAM		///< Instructs graphics library to use selected shader program
+	};
+
+	/// Command information
+	struct Clear
+	{
+		glm::vec4 color;		///< RGBA color buffer clear
+		unsigned int flags; ///< Bitmask for whether to clear color, depth, stencil
+	};
+
+	/// Command information
+	struct Draw
+	{
+		unsigned int indexCount;		///< Number of indices to be drawn
+		unsigned int startIndex;		///< Starting index
+		unsigned int primitiveType;	///< Triangles, quads, strips, lines, etc.
 	};
 
 	/**
@@ -45,27 +62,17 @@ namespace Commands
 	 * @author Seth Traman
 	 * @date Aug 2018
 	 *
-	 * @brief Intended for storing command information fields for every type of supported command
+	 * @brief Intended for storing command information fields for every supported command type
 	 * @todo Support more types of command data
 	 *
 	 */
 
 	union Data
 	{
+		Clear clear;
+		Draw draw;
+		unsigned int index;
 		const char * name;
-	};
-
-	struct Clear
-	{
-		glm::vec4 color;
-		unsigned int flags;
-	};
-
-	struct Draw
-	{
-		unsigned int indexCount;
-		unsigned int startIndex;
-		unsigned int primitiveType;
 	};
 }
 
@@ -78,18 +85,14 @@ namespace Commands
  *
  * @brief Structure describing an individual graphics library command
  *
- * @public command : The type of the command
- * @public data : Any data requireds for the execution of the command
- * @public resourceSlot : the ID of the resource being accessed, if any.  In OpenGL, this could be the ID of a texture, shader, VAO, or any such stateful object.
- *
  **/
 
 struct DeferredCommand
 {
-	Commands::Type command;
-	Commands::Data data;
+	Commands::Type command;			///< The type of command to be run
+	Commands::Data data;				///< Data required for execution of command
 
-	unsigned int resourceSlot;
+	unsigned int resourceSlot;	///< Location or ID of resource to be used during execution
 };
 
 }
