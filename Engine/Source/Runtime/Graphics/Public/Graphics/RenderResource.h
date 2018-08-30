c5f6af16ba4f3119f5efec6d5fdc684b438221a5
cellman1234 <sethtraman@gmail.com>
2018-08-29 20:01:27 -0500

    .

--- Engine/Source/Runtime/Graphics/Public/Graphics/RenderResource.h
+++ Engine/Source/Runtime/Graphics/Public/Graphics/RenderResource.h
@@ -7,8 +7,8 @@ namespace Graphics
 	template <uint32 T, uint32 U>
 	struct GenericHandle
 	{
-		uint32 version:	T;
-		uint32 index:	U;
+		uint32 version: T;
+		uint32 index: U;
 	};
 
 	using VertexArrayHandle = GenericHandle<16, 16>;
